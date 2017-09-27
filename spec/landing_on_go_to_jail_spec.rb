c08414a1906b8db9673411b9a75a4a5f12ed50bd
Ángel Sanz <angelsanzgit@gmail.com>
2017-09-27 12:36:00 +0200

    Write the correct expectation for the test about passing over Go To Jail

--- spec/landing_on_go_to_jail_spec.rb
+++ spec/landing_on_go_to_jail_spec.rb
@@ -5,29 +5,28 @@ describe 'Landing on Go To Jail' do
   it 'landing on Go To Jail ends up on Just Visiting' do
     player = FixedMovementPlayer.new
     player.locate('Indiana Avenue')
-    
+
     game = Game.new(player, player)
     game.next_turn
 
     expect(player.where).to eq('Just Visiting')
   end
 
-  it 'landing on Go To Jail dont change the balance' do
+  it 'landing on Go To Jail does not change the balance' do
     player = FixedMovementPlayer.new
     player.locate('Indiana Avenue')
-    
+
     game = Game.new(player, player)
 
     expect{ game.next_turn }.not_to change{ player.balance }
   end
 
-  it 'passing over Go To Jail dont change the balance' do
+  it 'passing over Go To Jail does not change the balance' do
     player = FixedMovementPlayer.new
     player.locate('Illinois Avenue')
-    
-    game = Game.new(player, player)
-    game.next_turn
 
-    expect(player.where).to eq('Pacific Avenue')
+    game = Game.new(player, player)
+
+    expect{ game.next_turn }.not_to change{ player.balance }
   end
 end
