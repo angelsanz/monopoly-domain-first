4255a6f7107d17766988f2333d53f53a12141297
Ángel Sanz <angelsanzgit@gmail.com>
2017-09-27 13:19:14 +0200

    Make play_a_round helper receive number of players

--- spec/winning_mechanics_spec.rb
+++ spec/winning_mechanics_spec.rb
@@ -7,7 +7,7 @@ describe 'Winning Mechanics' do
     competitor_one = FixedMovementPlayer.avoiding_luxury_tax_player
     competitor_two = FixedMovementPlayer.avoiding_luxury_tax_player
     loser = FixedMovementPlayer.loser
-    
+
     game = Game.new(loser, competitor_one, competitor_two)
 
     turns = []
@@ -21,33 +21,35 @@ describe 'Winning Mechanics' do
   it 'a player wins when every other player has lost' do
     winner = Player.new
     loser = FixedMovementPlayer.loser
+    players = [loser, winner]
 
-    game = Game.new(loser, winner)
+    game = Game.new(*players)
 
-    expect{ play_a_round(game) }.to raise_error('We got a winner!')
+    expect{ play_a_round(game, players.size) }.to raise_error('We got a winner!')
   end
 
   it 'a player wins when every other player has lost' do
     winner = Player.new
     loser = FixedMovementPlayer.loser
     another_loser = Player.new
+    players = [loser, winner, another_loser]
 
-    game = Game.new(loser, winner, another_loser)
+    game = Game.new(*players)
 
-    expect{ play_a_round(game) }.not_to raise_error('We got a winner!')
+    expect{ play_a_round(game, players.size) }.not_to raise_error('We got a winner!')
 
     make_lose(another_loser)
 
-    expect{ play_a_round(game) }.to raise_error('We got a winner!')
+    expect{ play_a_round(game, players.size) }.to raise_error('We got a winner!')
   end
 
   def make_lose(player)
     player.charge(100)
   end
 
-  def play_a_round(game)
-    game.next_turn
-    game.next_turn
-    game.next_turn 
+  def play_a_round(game, turns)
+    turns.times do
+      game.next_turn
+    end
   end
 end
